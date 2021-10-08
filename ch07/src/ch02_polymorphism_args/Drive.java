6f9e68702f7191ae507865715142c8dead565c71
kwh1205 <qhtmej1@nate.com>
2021-10-08 17:41:26 +0900

    ㅇㄴㅇㅇ

--- ch07/src/ch02_polymorphism_args/Drive.java
+++ ch07/src/ch02_polymorphism_args/Drive.java
@@ -2,9 +2,16 @@ package ch02_polymorphism_args;
 
 public class Drive {
 
-  void drive(Vehicle v) {//메소드의 매개변수에 다른클래스넣기가능
+  void drive(Vehicle v) {//메소드의 매개변수에 다른클래스넣기가능 //메소드의 매개변수의 다형성
+	  //Vehicle 타입으로 넣기위해서 뒤에있는v에 버스 , 트럭 Vehicle가 들어감
 	  v.run();
 	}
+  void drive(Vehicle[] vs) {
+	  for(Vehicle v:vs) {
+		  v.run();
+	  }
+	  
+  }
   
   public static void main(String[] args) {
 	  Drive drv = new Drive();
@@ -16,6 +23,15 @@ public class Drive {
 	  //매개변수의 다형성
 	  Truck truck = new Truck();
 	  drv.drive(truck);
+	  
+	  Vehicle[] vehicles = new Vehicle[4];
+	  vehicles[0]=v1;
+	  vehicles[1]=bus;
+	  vehicles[2]=truck;
+	  vehicles[3]=bus;
+	  
+	  drv.drive(vehicles);
+	 
   }
   
 }
